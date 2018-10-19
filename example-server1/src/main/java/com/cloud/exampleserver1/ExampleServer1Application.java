b209d7a0018ac7fb6b227f4a7c810b0bcecc800c
OKravtsiv <kravtsivostap@gmail.com>
2018-10-19 13:39:01 +0300

    Added example-server1

--- example-server1/src/main/java/com/cloud/exampleserver1/ExampleServer1Application.java
+++ example-server1/src/main/java/com/cloud/exampleserver1/ExampleServer1Application.java
@@ -1,15 +1,13 @@
-package com.cloud.configserver;
+package com.cloud.exampleserver1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableConfigServer
 @EnableEurekaClient
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ExampleServer1Application {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ExampleServer1Application.class, args);
     }
 }
