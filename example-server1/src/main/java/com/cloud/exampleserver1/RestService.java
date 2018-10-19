99712db2206d4e993ce18351b9c62c38b1492c38
OKravtsiv <kravtsivostap@gmail.com>
2018-10-19 17:51:33 +0300

    Rewrite properties on yml

--- example-server1/src/main/java/com/cloud/exampleserver1/RestService.java
+++ example-server1/src/main/java/com/cloud/exampleserver1/RestService.java
@@ -1,7 +1,18 @@
 package com.cloud.exampleserver1;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest")
 public class RestService {
+    @Value("${message: Default message}")
+    private String message;
+
+    @GetMapping("/message")
+    private String getMessage() {
+        return message;
+    }
 }
