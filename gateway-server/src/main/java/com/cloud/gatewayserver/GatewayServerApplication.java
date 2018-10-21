9291fd50fc731c2ec81083980d3b7ead3f67e863
OKravtsiv <kravtsivostap@gmail.com>
2018-10-21 22:35:24 +0300

    Working on auth

--- gateway-server/src/main/java/com/cloud/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/cloud/gatewayserver/GatewayServerApplication.java
@@ -2,10 +2,12 @@ package com.cloud.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 @EnableZuulProxy
 @EnableEurekaClient
 public class GatewayServerApplication {
---
+++ gateway-server/src/main/java/com/cloud/gatewayserver/config/GatewaySecurityConfigurer.java
@@ -0,0 +1,33 @@
+package com.cloud.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class GatewaySecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .permitAll()
+                .and()
+                .csrf()
+                .csrfTokenRepository(
+                        CookieCsrfTokenRepository
+                                .withHttpOnlyFalse());
+    }
+}
