688457097355a40d71a8b176601d2187024ad769
OKravtsiv <kravtsivostap@gmail.com>
2018-10-22 15:34:51 +0300

    Added auth server and resource server

--- gateway-server/src/main/java/com/cloud/gatewayserver/config/GatewaySecurityConfigurer.java
+++ gateway-server/src/main/java/com/cloud/gatewayserver/config/GatewaySecurityConfigurer.java
@@ -1,16 +1,13 @@
 package com.cloud.gatewayserver.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
+@Order(101)
 public class GatewaySecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
---
+++ resource-server/src/main/java/com/cloud/resourceserver/config/SecurityConfigurer.java
@@ -0,0 +1,20 @@
+package com.cloud.resourceserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfigurer extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
